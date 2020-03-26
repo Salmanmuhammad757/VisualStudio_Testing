d6ab875934d1a84d3aa7f6f5e761218e1f18d424
Muhammad Salman <54834285+Salmanmuhammad757@users.noreply.github.com>
2020-03-26 10:42:25 +0500

    Automation testing using c#

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("http://github.com/salmanmuhammad757");
+            Thread.Sleep(3000);
+
+       //     driver.FindElement(By.XPath("//span[@class='repo']")).Click();
+        //    Thread.Sleep(3000);
+
+            driver.FindElement(By.XPath("//a[@class='Salmanmuhammad757?tab=following']")).Click();
+            Thread.Sleep(3000);
+
+            driver.Quit();
+
+        }
+    }
+}
